fa5c08ff2f501a5f38aeb64a0c387e9c67c8693e
AgentToast <b.huhta@hotmail.com>
2018-10-03 12:51:55 -0500

    Added Gameboard constructor.

--- OthelloPlayer.Startup/Game/Gameboard.cs
+++ OthelloPlayer.Startup/Game/Gameboard.cs
@@ -1,16 +1,33 @@
-﻿namespace OthelloPlayer.Startup.Game
+﻿using System;
+
+namespace OthelloPlayer.Startup.Game
 {
     public class Gameboard
     {
         #region Properties
 
-        
+        public int Size { get; }
+        public string WhiteToken { get; } = "O";
+        public string BlackToken { get; } = "X";
 
         #endregion
 
         #region Constructor
 
-        
+        public Gameboard(int size)
+        {
+            if (size < 4)
+            {
+                throw new ArgumentException($"{nameof(size)} is {size}; expected to be at least 4.");
+            }
+
+            if (size % 2 != 0)
+            {
+                throw new ArgumentException($"{nameof(size)} is {size}; expected to be an even value.");
+            }
+
+            Size = size;
+        }
 
         #endregion
 
